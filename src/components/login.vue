533bcf47f58e4f09c2b9aad2e0bd7b34d0659934
kubi <2306302720@qq.com>
2020-09-27 15:03:12 +0800

    chajioan

---
+++ src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+     <div class="login_container">
+       <!-- 图像区 -->
+       <div class="login_avatar">
+         <img src="./assets/logo.png" alt="">
+       </div>
+       <!-- 表单区 -->
+     <div class="login_box">
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="less" scoped>
+
+  .login_container {
+    background-color: #2b4b6b;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lintOnSave: false,
+
+    devServer: {
+        open: true,
+        port: 8878
+    }
+
+}
